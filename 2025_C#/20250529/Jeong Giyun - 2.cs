ac508192e90b79326a3ac8da03ec272a73bf0506
JEONG GIYOON <jgy5914@naver.com>
2025-05-29 22:01:44 +0900

    2025/05/29 JeongGiyun - 2

---
+++ 2025_C#/20250529/Jeong Giyun - 2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public double Score;
+
+    public Student(string name, double score)
+    {
+        Name = name;
+        Score = score;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] students = new Student[5]
+        {
+            new Student("김민재", 87.5),
+            new Student("이지은", 92.3),
+            new Student("유재석", 55.0),
+            new Student("정해인", 75.4),
+            new Student("장원영", 98.5)
+        };
+
+        double total = 0;
+        int passCount = 0;
+
+        foreach (var s in students)
+        {
+            total += s.Score;
+            if (s.Score >= 60)
+                passCount++;
+        }
+
+        Console.WriteLine($"총점: {total}점");
+        Console.WriteLine($"평균: {total / students.Length}점");
+        Console.WriteLine($"합격자 수: {passCount}명");
+    }
+}
